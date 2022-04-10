4eaa68e01d28ed97da402fb8b00f06b26b585573
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-04-10 12:49:18 -0300

    Implementa botão de voltar para Home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,30 @@
+import './Header.css';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import logoGithub from './images/icons8-github.svg';
 function Header() {
+  const  history  = useHistory();
+  const { pathname } = history.location;
+
+  const returningForHome = () => {
+    history.push('/')
+  }
+
  return (
   <header>
-    <img src={ logoGithub } alt="logo-github"/>
-    <h3>Github</h3>
-    <p>profiles</p>
+    <div className='logo-container'>
+      <img src={ logoGithub } alt="logo-github"/>
+      <h3>Github</h3>
+      <p>profiles</p>
+    </div>
+    { pathname === '/repos' && (
+      <button 
+        className='btn-change-user'
+        onClick={ returningForHome }
+      >
+        CHANGE USERNAME
+      </button>
+    )}
+  
   </header> 
  )
 }
