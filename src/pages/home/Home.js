d10b21be1eb5c910d57b3a512c73f159d8b6b7d5
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-04-10 10:42:27 -0300

    Altera  stilos ja criados

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import MyContext from '../../context/MyContext';
 import getPerfil from '../../util/getPerfil';
 import getRepos from '../../util/getRepos';
+import Header from '../../components/Header/Header';
 function Home() {
 
   const history = useHistory();
@@ -30,27 +31,27 @@ function Home() {
 
   return (
    <div className='container'>
-    <header>
-      <img src="./images/icons8-github-30.png" alt="logo-github"/>
+     <Header/>
+    {/* <header>
+      <img src={ logoGithub } alt="logo-github"/>
       <h3>Github</h3>
       <p>profiles</p>
-    </header>
-    <main>
+    </header> */}
+    <main className='main-content'>
       <form onSubmit={ (e) => { handleSubmit(e) } }>
-     <div className='input-container'>
-      <i class="bi bi-zoom-in"></i>
-      <input 
-        value={ user }
-        onChange={ handleInputChange }
-        className='search-users' 
-        placeholder='Insert a username' 
-        type="text"/>
-      </div>
+        <div className='input-container'>
+          <i class="bi bi-search"></i>
+          <input 
+          className='search-users' 
+          value={ user }
+          onChange={ handleInputChange }
+          placeholder='Insert a username' 
+          type="text"/>
+        </div>
         {/* <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon">
           <path fill-rule="evenodd" d="M11.5 7a4.499 4.499 0 11-8.998 0A4.499 4.499 0 0111.5 7zm-.82 4.74a6 6 0 111.06-1.06l3.04 3.04a.75.75 0 11-1.06 1.06l-3.04-3.04z"></path>
         </svg> */}
       </form>
-      
     </main>
    </div> 
   )
