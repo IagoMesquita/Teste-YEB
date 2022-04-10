29e494dc64e62ae4899020fe800d926c39e7eea0
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-04-09 21:05:58 -0300

    Implementa contagem de repos e starred

--- src/pages/repos/Repo.js
+++ src/pages/repos/Repo.js
@@ -11,53 +11,66 @@ function Repos () {
   
 
   console.log(profile)
-  return(
-   <div>
-     <header>
-      <img src="./images/icons8-github-30.png" alt="logo-github"/>
-      <h3>Github</h3>
-      <p>profiles</p>
-    </header>
-    <section className="profile-container">
-      <img width={61} src={  profile.avatar_url } alt="incone-profile"/>
-      <div>
-        <span>{ profile.name }</span>
-        <div>{ profile.bio }</div>
+  return (
+    <div>
+      <header>
+        <img src="./images/icons8-github-30.png" alt="logo-github" />
+        <h3>Github</h3>
+        <p>profiles</p>
+      </header>
+      <section className="profile-container">
+        <img width={ 61 } src={ profile.avatar_url } alt="incone-profile" />
+        <div>
+          <span>{ profile.name }</span>
+          <div>{ profile.bio }</div>
+        </div>
+      </section>
+      <div className="button-container">
+        <button className="button-filter">
+          <span>Repos</span>
+          <span className='button-count'>{ repos.length }</span>
+        </button>
+        <button className="button-filter">
+          <span>Starred</span>
+          <span className='button-count'>{
+          repos.filter((repo) => 
+            (repo.stargazers_count === 1 )).length
+          }
+          </span>
+        </button>
       </div>
-    </section>
-    <div className="button-container">
-      <button>Repos</button>
-      <button>Start</button>
+      <main className="repos-container">
+        <div className="input-container">
+          <i class="bi bi-zoom-in"></i>
+          <input
+            value={ nameRepo }
+            onChange={({ target }) => setNameRepo(target.value)}
+            className="search-users"
+            placeholder="Filter by name"
+            type="text"
+          />
+        </div>
+        {repos
+          .filter((repo) => repo.name.includes(nameRepo))
+          .map((repo) => (
+            <div className="repos-text" key={ repo.id }>
+              <span>{ repo.name }</span>
+              <span>{ repo.description }</span>
+              <div className="icon-container">
+                <div className="icons">
+                  <strong>{ "< >" }</strong>
+                  <span>{ repo.language }</span>
+                </div>
+                <div className="icons">
+                  <img src={ forkImage } alt="icon-fork" />
+                  <span>{ repo.forks_count }</span>
+                </div>
+              </div>
+            </div>
+          ))}
+      </main>
     </div>
-    <main className='repos-container'>
-    <div className='input-container'>
-      <i class="bi bi-zoom-in"></i>
-      <input 
-        value={ nameRepo }
-        onChange={ ({ target }) => setNameRepo(target.value)  }
-        className='search-users' 
-        placeholder='Filter by name' 
-        type="text"/>
-      </div>
-      { repos.filter((repo) => repo.name.includes(nameRepo) ).map((repo) => (
-        <div className='repos-text' key={ repo.id }>
-          <span>{ repo.name }</span>
-          <span>{ repo.description }</span>
-          <div className='icon-container'>
-            <div className='icons'>
-              <strong>{ '< >' }</strong>
-              <span>{ repo.language }</span>              
-            </div>
-            <div className='icons'>
-              <img src={ forkImage } alt="icon-fork"/>
-              <span>{ repo.forks_count }</span>
-            </div>
-          </div>
-      </div>
-      )) }
-    </main>
-   </div>
-  )
+  );
 }
 
 export default Repos;
