331cc5179b7ba09d88971519164cca47a48989af
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-04-12 00:21:10 -0300

    Finaliza botes de filtro

--- src/pages/repos/Repo.js
+++ src/pages/repos/Repo.js
@@ -4,13 +4,14 @@ import Header from '../../components/Header/Header';
 import Profile from '../../components/Profile/Profile';
 import Starreds from '../../components/Starred/Starred';
 import AllRepos from '../../components/AllRepos/AllRepo';
+import ButtonsRepoAndStarred from './ButtonsReposAndStarred/ButtonsRepoAndStarred';
 
 import './Repo.css';
 function Repos () {
 
   const { 
-    repos, nameRepo, setNameRepo, 
-    repoAllOrStar, setRepoAllOrStar,
+     nameRepo, setNameRepo, 
+    repoAllOrStar,
     loading,
   } = useContext(MyContext)
 
@@ -27,20 +28,7 @@ function Repos () {
     <div>
       <Header/>
       <Profile/>
-      <div className="button-container">
-        <button className="button-filter" onClick={ () => setRepoAllOrStar(true) }>
-          <span>Repos</span>
-          <span className='button-count'>{ repos.length }</span>
-        </button>
-        <button className="button-filter" onClick={ () => setRepoAllOrStar(false) }>
-          <span>Starred</span>
-          <span className='button-count'>{
-          repos.filter((repo) => 
-            (repo.stargazers_count >= 1 )).length
-          }
-          </span>
-        </button>
-      </div>
+      <ButtonsRepoAndStarred/>
       <main className="repos-container">
         <div className="input-container">
           <i class="bi bi-search"></i>
@@ -54,24 +42,6 @@ function Repos () {
         </div>
         { repoAllOrStar ? (
           <AllRepos/>
-          // repos
-          //   .filter((repo) => repo.name.includes(nameRepo))
-          //   .map((repo) => (
-          //     <div className="repos-content" key={ repo.id }>
-          //       <span>{ repo.name }</span>
-          //       <span>{ repo.description }</span>
-          //       <div className="icon-container">
-          //         <div className="icons">
-          //           <strong>{ "< >" }</strong>
-          //           <span>{ repo.language }</span>
-          //         </div>
-          //         <div className="icons">
-          //           <img src={ forkImage } alt="icon-fork" />
-          //           <span>{ repo.forks_count }</span>
-          //         </div>
-          //       </div>
-          //     </div>
-          //   ))
         )
         : ( <Starreds/>) 
         }
